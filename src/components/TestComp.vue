a3b29c6e031d43d26b73dd40ae7e2141822b5e55
YonDee <im.yondee@gmail.com>
2022-02-09 17:04:23 +0800

    配置环境

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
     测试插件
+    {{NODE_ENV_TYPE}}
   </div>
 </template>
+
+<script setup>
+import {reactive} from "vue"
+let NODE_ENV_TYPE = reactive(NODE_ENV)
+</script>
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,37 +1,53 @@
+const fs = require('fs')
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
+const dotenv = require('dotenv');
+const webpack = require("webpack")
+const PROJECT_PATH = path.resolve(__dirname, "../");
+const dotenvFile = path.resolve(PROJECT_PATH, `./.env.${process.env.NODE_ENV}`);
+dotenv.config({
+  path: fs.existsSync(dotenvFile)
+    ? dotenvFile
+    : resolve(PROJECT_PATH, `./.env`),
+});
+console.log("【process.env】", process.env.ENV);
 
-module.exports = {
-  mode: "development",
-  devtool: 'eval-cheap-module-source-map',
-  entry: './src/main.js',
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      }
-    ]
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      title: "yangdi's blog"
-    }),
-    new CleanWebpackPlugin(),
-    new VueLoaderPlugin()
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+module.exports = env => {
+  return {
+    mode: "development",
+    devtool: 'eval-cheap-module-source-map',
+    entry: './src/main.js',
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        }
+      ]
     },
-    compress: true,
-    port: 9000,
-  },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        title: "yangdi's blog"
+      }),
+      new CleanWebpackPlugin(),
+      new VueLoaderPlugin(),
+      new webpack.DefinePlugin({
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 9000,
+    },
+  }
 };
