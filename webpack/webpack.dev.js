c61c8a88ada666fb34eb600a89659ea574aa4a4e
YonDee <im.yondee@gmail.com>
2022-02-10 16:28:00 +0800

    添加全局样式

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,13 +1,15 @@
-const fs = require('fs')
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const { VueLoaderPlugin } = 
-require('vue-loader')
-const dotenv = require('dotenv');
-const webpack = require("webpack")
+const fs = require("fs");
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+
+const dotenv = require("dotenv");
+const webpack = require("webpack");
 const PROJECT_PATH = path.resolve(__dirname, "../");
 const dotenvFile = path.resolve(PROJECT_PATH, `./.env.${process.env.NODE_ENV}`);
+
 dotenv.config({
   path: fs.existsSync(dotenvFile)
     ? dotenvFile
@@ -15,60 +17,71 @@ dotenv.config({
 });
 console.log("【process.env】", process.env.ENV);
 
-module.exports = env => {
+module.exports = (env) => {
   return {
     mode: "development",
-    devtool: 'eval-cheap-module-source-map',
-    entry: './src/main.js',
+    devtool: "eval-cheap-module-source-map",
+    entry: "./src/main.js",
     module: {
       rules: [
         {
           test: /\.vue$/,
-          loader: 'vue-loader'
+          loader: "vue-loader",
         },
         {
           test: /\.s[ac]ss$/i,
-          use: [{
-              loader: "style-loader" // 将 JS 字符串生成为 style 节点
-          }, {
-              loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
-          }, {
-              loader: "sass-loader" // 将 Sass 编译成 CSS
-          }],
+          use: [
+            {
+              loader: "style-loader", // 将 JS 字符串生成为 style 节点
+            },
+            {
+              loader: "css-loader", // 将 CSS 转化成 CommonJS 模块
+            },
+            {
+              loader: "sass-loader", // 将 Sass 编译成 CSS
+            },
+          ],
         },
         {
           test: /\.css$/,
-          use: [ 'style-loader', 'css-loader' ]
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.mjs$/i,
-          resolve: { byDependency: { esm: { fullySpecified: false } } },
+          resolve: {
+            byDependency: {
+              esm: {
+                // ES modules（ESM） 是 JavaScript 官方的标准化模块系统。
+                fullySpecified: false, // 关闭 resolve.fullySpecified
+              },
+            },
+          },
           include: /node_modules/,
-          type: "javascript/auto"
-        }
-    ]
+          type: "javascript/auto",
+        },
+      ],
     },
     output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: 'public/index.html',
-        title: "yangdi's blog"
+        template: "public/index.html",
+        title: "yangdi's blog",
       }),
       new CleanWebpackPlugin(),
       new VueLoaderPlugin(),
       new webpack.DefinePlugin({
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
       }),
     ],
     devServer: {
       static: {
-        directory: path.join(__dirname, 'public'),
+        directory: path.join(__dirname, "public"),
       },
       compress: true,
       port: 9000,
     },
-  }
+  };
 };
